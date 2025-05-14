3b7041309829dcad0cf106fe8eeda439ea3c8ca6
Puwadech Intong <noonemore.gg@gmail.com>
2025-05-14 10:16:19 +0700

    Yes, Initial commit

---
+++ src/main/java/net/byebye007x/ourhomemod/Client.java
@@ -0,0 +1,10 @@
+package net.byebye007x.ourhomemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/byebye007x/ourhomemod/OurHomeMod.java
@@ -0,0 +1,16 @@
+package net.byebye007x.ourhomemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OurHomeMod implements ModInitializer {
+	public static final String MOD_ID = "ourhomemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
